5560418d6dbe5367c32210e232333182d6fda5c4
Ivelin Penchev <ivelinpenchev@gmail.com>
2018-02-03 18:19:00 +0000

    Added global trend in training data

--- data_distributor/aggregator.py
+++ data_distributor/aggregator.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import threading
 import pymongo
+from scipy import stats
 
 def mapper(t):
     return float(t["high"]) + float(t["low"]) / 2
@@ -14,30 +15,57 @@ class Aggregator:
 
         self.thread = threading.Thread(target=self.run, args=())
         self.thread.daemon = True
-        self.thread.start()
+        # self.thread.start()
+        self.run()
 
     def tick(self):
         network_input = self.global_config["neural_network"]["input"]
         network_output = self.global_config["neural_network"]["output"]
-        data = np.empty((0, network_input + network_output))
+        currency_data = {}
 
         print("saving data")
+        shortest_data_count = 0
         for currency in self.currency_config.keys():
-            since_timestamp = 0
-            klines_data = np.array(list(self.db.get(currency, "klines").find().sort("timestamp", pymongo.ASCENDING)))
-            count = len(klines_data)
-            klines_data = np.array([mapper(x) for x in klines_data])
+            klines_data = list(self.db.get(currency, "klines").find().sort("timestamp", pymongo.ASCENDING).limit(2*1440))
+            klines_data = [mapper(x) for x in klines_data]
+            currency_data[currency] = klines_data
+            if len(klines_data) < shortest_data_count or shortest_data_count == 0:
+                print(currency, len(klines_data))
+                shortest_data_count = len(klines_data)
+        
+        print(shortest_data_count)
+        lin_regression_x = np.arange(network_input)
+        data_per_currency = shortest_data_count - network_input - 61
+        num_of_currencies = len(self.currency_config.keys())
+        data = np.empty((data_per_currency * num_of_currencies , network_input + network_output + 1))
 
-            for i in range(0, count - network_input - 60):
-                input_data = klines_data[i:i + network_input]
+        for i in range(0, data_per_currency):
+            # calculate the global trend
+            slopes = []
+            for currency in currency_data.keys():
+                slope = stats.linregress(lin_regression_x, currency_data[currency][i:i + network_input])
+                slopes.append(slope)
+
+            general_trend = np.average(slopes)
+            if general_trend > 0:
+                print(general_trend)
+
+            j = 0
+            for currency in currency_data.keys():
+                input_data = currency_data[currency][i:i + network_input + 61]
 
                 label_data = np.array([])
-                label_data = np.append(label_data, klines_data[i + network_input + 1])
-                label_data = np.append(label_data, klines_data[i + network_input + 60])
+                label_data = np.append(label_data, input_data[network_input + 1])
+                label_data = np.append(label_data, input_data[network_input + 60])
+
+                # normalize the data
+                input_data = input_data[:network_input] / np.max(input_data[:network_input])
+                input_data = np.append(input_data, general_trend)
 
                 result = np.append(input_data, label_data)
                 result = result / np.max(result[:network_input])
-                data = np.vstack((data, result))
+                data[i * num_of_currencies + j] = result
+                j+=1
 
         np.save(self.global_config["neural_network"]["training_file"], data)
         print(str(data.shape) + " sets of data saved")
